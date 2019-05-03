62af61f3b5fd7d4dc7b7876dba10006a39af0e83
senthil3105 <50225596+senthil3105@users.noreply.github.com>
2019-05-03 09:36:36 +0530

    Add files via upload

---
+++ test1.php
@@ -0,0 +1,30 @@
+use DBI;
+
+$dbh = DBI->connect( "dbi:ODBC:beemail") || die "Cannot connect:     $DBI::errstr";
+=pod
+## Select based on user input
+print "\nDisplaying your received mails \n";
+print "Enter  from email address ";
+$fromemail = <>;
+chomp($fromemail);
+print "Enter  to  email address ";
+$toemail = <>;
+chomp($toemail);
+=cut
+
+my $sth = $dbh->prepare("select body from mails"); # ? is used to denote that the value can be changed
+$sth->execute() or die $DBI::errstr;
+
+while (my @row = $sth->fetchrow_array()) {
+   my ($line)=@row;
+   if(($greeting,$name,$body)=$line=~ m/^(hi|hello|dear)\sdr\.(\w+)\n(.+|\n+)/ix)
+	{
+	# print "$line\n";
+	 print "nick : $name\n end : $body\n";
+	 
+	# $`, $&, $'
+	+
+	# print "regards : $regards\n ";
+	 }
+}
+$sth->finish();
